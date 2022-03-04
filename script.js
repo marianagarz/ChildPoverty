0de76685f55e46af3fd6b9c8470d9374959b9079
Shriver <mshriver@FRIENDSBALT.ORG>
2022-03-04 12:57:07 -0500

    table work

--- script.js
+++ script.js
@@ -30,24 +30,25 @@ function toArray(text){
 	const rows= text.split("\n"); 
 	
 	for (i=0;i<rows.length; i++){
-		console.log(rows[i]);
-		const temp= rows[i].split(",");		
+		const temp= rows[i].split(",");	
+		console.log(temp);		
 		const obj= {"state": temp[0],"fips":temp[1],"id":temp[2],"name":temp[3],"totalPop":temp[4],"schoolAgedPop":temp[5],"schoolInPov":temp[6],"povRate":((temp[6]/temp[5])*100)};
 		fullTable.push(obj);
 	}
 	
-	document.getElementById("state").innerHTML = fullTable[1].state;
-
+	toTable(fullTable);
+	
 }
 
-function toTable(array[])
+function toTable(array)
 {
-	let html="<table> <th><td>State</td><td>FIPS Number</td><td>ID</td><td>Name</td><td>Total Population</td> <td>School aged population</td><td> School aged pop in poverty</td><td>Povrate</td></th>";
+	let html="<table> <td>State</td><td>FIPS Number</td><td>ID</td><td>Name</td><td>Total Population</td> <td>School aged population</td><td> School aged pop in poverty</td><td>Povrate</td>";
 	for (i=0;i<array.length; i++){
-		html+=("<tr><td>"+array[i].state+"</td><td>"+array[i].id+"</td><td>ID</td><td>Name</td><td>Total Population</td> <td>School aged population</td><td> School aged pop in poverty</td><td>Povrate</td> "
+		html+=("<tr><td>"+array[i].state+"</td><td>"+array[i].fips+"</td><td> "+array[i].id+" </td><td> "+array[i].name+" </td><td>"+array[i].totalPop+" </td> <td>"+array[i].schoolAgedPop+" </td><td> "+array[i].schoolInPov+" </td><td>"+array[i].povRate+" </td> ")
 	}
+	html+=("</table>");
+	document.getElementById("table").innerHTML = html;
 
-	
 }
 
 
