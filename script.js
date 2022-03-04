3a85e6d6176ac3ff9e9d6b6b3baf0aa47aa189c2
Garza <mgarza@friendsbalt.org>
2022-03-04 12:22:46 -0500

    most updated version oops!!!

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 function loadDoc() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        document.getElementById("demo").innerHTML = this.responseText;
+       
 		toArray(this.responseText);
     }
     xhttp.open("GET", "Data.txt", true);
@@ -28,17 +28,28 @@ function toArray(text){
 	const fullTable=[]; 
 	
 	const rows= text.split("\n"); 
-	console.log(rows);
 	
-	for (i=0;i<=rows.length; i++){
-		const temp= rows[i].split(","); 
-		const obj= {"state": temp[0]};
+	for (i=0;i<rows.length; i++){
+		console.log(rows[i]);
+		const temp= rows[i].split(",");		
+		const obj= {"state": temp[0],"fips":temp[1],"id":temp[2],"name":temp[3],"totalPop":temp[4],"schoolAgedPop":temp[5],"schoolInPov":temp[6],"povRate":((temp[6]/temp[5])*100)};
 		fullTable.push(obj);
 	}
 	
-	document.getElementById("test").innerHTML = fullTable[0];
+	document.getElementById("state").innerHTML = fullTable[1].state;
 
 }
+
+function toTable(array[])
+{
+	let html="<table> <th><td>State</td><td>FIPS Number</td><td>ID</td><td>Name</td><td>Total Population</td> <td>School aged population</td><td> School aged pop in poverty</td><td>Povrate</td></th>";
+	for (i=0;i<array.length; i++){
+		html+=("<tr><td>"+array[i].state+"</td><td>"+array[i].id+"</td><td>ID</td><td>Name</td><td>Total Population</td> <td>School aged population</td><td> School aged pop in poverty</td><td>Povrate</td> "
+	}
+
+	
+}
+
 
 
   
