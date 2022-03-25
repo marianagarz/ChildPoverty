fda2b5db62364eea0815faf400b9d93fd6988e89
Garza <mgarza@friendsbalt.org>
2022-03-25 09:38:59 -0400

    with masons changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let length=0;  
 let mode=0; 
+let fwoop=1;
 const fullTable=[];  
 
 function myFunction() {
@@ -75,7 +76,109 @@ function toTable(array)
 
 }
 
-loadDoc();
 
+function search()
+{
+	let column =document.getElementById("search-column").getValue();
+	let mode =document.getElementById("search-scope").getValue();
+	//iffy on this tbh (c below);
+	let ident =document.getElementById("searchfor").getValue();
+	
+	toTable(fullTable.filter(getSearchCallBack(column,mode.ident))); 
+}
+
+	
+function getSearchCallBack(column, mode,ident){
+	const seachModes= [ (currentValue) => (currentValue[column])==ident, 
+						(currentValue) => (currentValue[column]).includes(ident),
+						(currentValue) => !(currentValue[column]).includes(ident)];
+	return searchModes[mode];
+}
+
+function sortState() {
+	fwoop++;
+	fullTable.sort(function(a, b){
+    if(a.state < b.state) { return -1; }
+    if(a.state > b.state) { return 1; }
+    return 0;
+})
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+	
+}
+function sortFips() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.fips - b.fips});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortID() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.id - b.id});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortName() {
+	fwoop++;
+	fullTable.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortTotalPop() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.totalPop - b.totalPop});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortSchoolAgedPop() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.schoolAgedPop - b.schoolAgedPop});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortSchoolInPov() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.schoolInPov - b.schoolInPov});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function sortPovRate() {
+	fwoop++;
+	fullTable.sort(function(a, b){return a.povRate - b.povRate});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function numer(type){
+	fwoop++;
+	fullTable.sort(function(a, b){return a.type - b.type});
+
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+
+function alpha(type){
+	fwoop++;
+	fullTable.sort(function(a, b){
+		if(a.type < b.type) { return -1; }
+		if(a.type > b.type) { return 1; }
+		return 0;
+})
+	if(fwoop % 2 == 0) {toTable(fullTable)}
+	else {toTable(fullTable.reverse())}
+}
+function Reset() {
+	toTable(fullTable);
+	changeDisplay(10);
+}
   
-  
+loadDoc();
