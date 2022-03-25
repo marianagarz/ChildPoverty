ebdd48147a2e7798755ccb750d4db57ff34ea24c
Garza <mgarza@friendsbalt.org>
2022-03-25 09:47:05 -0400

    mason and mariana last night work

--- script.js
+++ script.js
@@ -76,6 +76,28 @@ function toTable(array)
 
 }
 
+function search()
+{
+	let column =document.getElementById("search-column").getValue();
+	let mode =document.getElementById("search-scope").getValue();
+	//iffy on this tbh (c below);
+	let ident =document.getElementById("searchfor").getValue();
+	
+	toTable(fullTable.filter(getSearchCallBack(column,mode.ident))); 
+}
+
+	//NOTE: might be a problem here... do i need to make a temp 
+	//array to sort by the collumn because rn its rows of objects 
+	//not sorted by collumns, or can i use a dot opperator 
+	// and make the opperator a variable??? thats how i have it rn, 
+	//other alternative would be to make a loop in search.
+function getSearchCallBack(column, mode,ident){
+	const seachModes= [ (currentValue) => (currentValue[column])==ident, 
+						(currentValue) => (currentValue[column]).includes(ident),
+						(currentValue) => !(currentValue[column]).includes(ident)];
+	return searchModes[mode];
+}
+
 
 function search()
 {
